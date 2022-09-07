e9bd230b69bc0d3485662e26f78b6b89689a1b25
EthanKim <hey0507@gmail.com>
2022-09-07 18:24:58 +0900

    :memo: Docs : Spring RestDocs api 등록
    Spring RestDocs api 를 이용한 테스트코드 문서화 작업 진행

---
+++ src/test/java/com/isang/api/controller/PostControllerDocTest.java
@@ -0,0 +1,66 @@
+package com.isang.api.controller;
+
+import com.isang.api.request.PostCreate;
+import com.isang.api.service.PostService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ExtendWith(RestDocumentationExtension.class)
+public class PostControllerDocTest {
+
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private PostService postService;
+
+
+    @BeforeEach
+    public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
+
+    @Test
+    @DisplayName("글 단건 조회 테스트")
+    void getPostRestDocTest() throws Exception {
+
+        //given
+        PostCreate postCreate =
+                PostCreate
+                        .builder()
+                        .title("하이염")
+                        .content("바이염")
+                        .build();
+
+
+        postService.write(postCreate);
+
+
+        //expected
+        this.mockMvc.perform(get("/posts/{postId}",1L)
+                .accept(APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("index"));
+    }
+
+}
