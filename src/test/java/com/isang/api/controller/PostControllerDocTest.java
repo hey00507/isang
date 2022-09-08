b37f9047ab16d0b6f69ef36b2292dc589cd981c7
EthanKim <hey0507@gmail.com>
2022-09-08 23:00:56 +0900

    :memo: Dosc: test 코드에 Asciidocs 적용

--- src/test/java/com/isang/api/controller/PostControllerDocTest.java
+++ src/test/java/com/isang/api/controller/PostControllerDocTest.java
@@ -1,5 +1,6 @@
 package com.isang.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.isang.api.request.PostCreate;
 import com.isang.api.service.PostService;
 import org.junit.jupiter.api.DisplayName;
@@ -14,9 +15,12 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,6 +30,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.isang.com", uriPort = 443)
 public class PostControllerDocTest {
 
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Autowired
     private MockMvc mockMvc;
@@ -36,7 +42,7 @@ public class PostControllerDocTest {
 
 
     @Test
-    @DisplayName("글 단건 조회 테스트")
+    @DisplayName("글 단건 조회")
     void getPostRestDocTest() throws Exception {
 
         //given
@@ -56,10 +62,44 @@ public class PostControllerDocTest {
                 .accept(APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andDo(document("index", pathParameters(
+                .andDo(document("post-inquiry",
+                        pathParameters(
                                 parameterWithName("postId").description("게시글 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("게시글 ID"),
+                                fieldWithPath("title").description("제목"),
+                                fieldWithPath("content").description("내용")
                         )
                 ));
     }
+
+    @Test
+    @DisplayName("글 등록")
+    void postPostRestDocTest() throws Exception {
+
+        //given
+        PostCreate request = PostCreate.builder()
+                .title("제목입니다.")
+                .content("내용입니다요.")
+                .build();
+
+        String json = objectMapper.writeValueAsString(request);
+
+        //expected
+        this.mockMvc.perform(post("/posts",1L)
+                        .contentType(APPLICATION_JSON) //accept 와 contentType 에 대한 차이점 확인하기
+                        .content(json))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("post-create",
+                        requestFields(
+                                fieldWithPath("title").description("제목")
+                                        .attributes(key("constraint").value("좋은 제목 입력해주세요. (바보는 안됨)")),
+                                fieldWithPath("content").description("내용").optional()
+                        )
+                ));
+    }
+
 
 }
