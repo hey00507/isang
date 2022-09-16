394f3977e6d35c253c11a5a8fdf27b9b38039d12
EthanKim <hey0507@gmail.com>
2022-09-16 20:06:31 +0900

    Test : Get Beans, new String initialize - 빈 목록 순회하기 - 스트링 선언방법

--- src/test/java/com/isang/IsangApplicationTests.java
+++ src/test/java/com/isang/IsangApplicationTests.java
@@ -2,12 +2,34 @@ package com.isang;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootTest
 class IsangApplicationTests {
 
+    static String test =
+                    """
+                    안녕
+                    하십니까
+                    이건 
+                    테스트 입니다.
+                    
+                    핳
+                    
+                    """;
     @Test
     void contextLoads() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println(test);
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(IsangApplication.class);
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = applicationContext.getBean(beanDefinitionName);
+            System.out.println("bean = " + bean);
+        }
     }
 
 }
